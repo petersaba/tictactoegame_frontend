3a68bc35713e35448cf29429357f78fbf9f97094
Peter Saba <petersaba001@gmail.com>
2022-09-16 17:35:34 +0300

    finished function to reset the game when there is a winner

--- javascript/tictactoe.js
+++ javascript/tictactoe.js
@@ -1,14 +1,21 @@
 let turn = 1
-const alreadyChosen = {
-    "square1" : false,
-    "square2" : false,
-    "square3" : false,
-    "square4" : false,
-    "square5" : false,
-    "square6" : false,
-    "square7" : false,
-    "square8" : false,
-    "square9" : false
+let alreadyChosen = resetAlreadyChosen()
+
+
+// resets already chosen object for the next game
+function resetAlreadyChosen(){
+    const alreadyChosen = {
+        "square1" : false,
+        "square2" : false,
+        "square3" : false,
+        "square4" : false,
+        "square5" : false,
+        "square6" : false,
+        "square7" : false,
+        "square8" : false,
+        "square9" : false
+    }
+    return alreadyChosen
 }
 
 function choose(event){
@@ -17,10 +24,20 @@ function choose(event){
         if (turn == 1){
             target.classList.add("red")
             alreadyChosen[target.id] = "red"
+            if (checkVictory("red")){
+                resetGame()
+                console.log(alreadyChosen)
+            }
+            // turn still changes so that the loser can start in the next game
             turn = 2
         }else{
             target.classList.add("yellow")
             alreadyChosen[target.id] = "yellow"
+            if (checkVictory("yellow")){
+                resetGame()
+                console.log(alreadyChosen)
+            }
+            // turn still changes so that the loser can start in the next game
             turn = 1
         }
     }
@@ -39,6 +56,17 @@ function checkVictory(color){
         (alreadyChosen["square1"] == color && alreadyChosen["square5"] == color && alreadyChosen["square9"] == color) ||
         (alreadyChosen["square3"] == color && alreadyChosen["square5"] == color && alreadyChosen["square7"] == color)) {
 
+        console.log(color);
         return color
     }
+}
+
+function resetGame(){
+    // remove the chips from all the squares
+    allSquares = document.querySelectorAll(".square")
+    for(const i of allSquares){
+        i.classList.remove("red")
+        i.classList.remove("yellow")
+    }
+    alreadyChosen = resetAlreadyChosen()
 }
