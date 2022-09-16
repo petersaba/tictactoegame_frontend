14be506383ec70ddd74e40e861bde15c5e75f35f
Peter Saba <petersaba001@gmail.com>
2022-09-16 17:18:02 +0300

    finished function to check if there is a winner

--- javascript/tictactoe.js
+++ javascript/tictactoe.js
@@ -25,3 +25,20 @@ function choose(event){
         }
     }
 }
+
+function checkVictory(color){
+    // first 3 are for the win with 3 of the same color in the same row
+    if((alreadyChosen["square1"] == color && alreadyChosen["square2"] == color && alreadyChosen["square3"] == color) || 
+        (alreadyChosen["square4"] == color && alreadyChosen["square5"] == color && alreadyChosen["square6"] == color) ||
+        (alreadyChosen["square7"] == color && alreadyChosen["square8"] == color && alreadyChosen["square9"] == color) ||
+        // these 3 are for the wins with the 3 of the same color in the same column
+        (alreadyChosen["square1"] == color && alreadyChosen["square4"] == color && alreadyChosen["square7"] == color) ||
+        (alreadyChosen["square2"] == color && alreadyChosen["square5"] == color && alreadyChosen["square8"] == color) ||
+        (alreadyChosen["square3"] == color && alreadyChosen["square6"] == color && alreadyChosen["square9"] == color) ||
+        // these 2 are for the win with 3 of the same color in the one of the diagonals
+        (alreadyChosen["square1"] == color && alreadyChosen["square5"] == color && alreadyChosen["square9"] == color) ||
+        (alreadyChosen["square3"] == color && alreadyChosen["square5"] == color && alreadyChosen["square7"] == color)) {
+
+        return color
+    }
+}
